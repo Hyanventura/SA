c06f010532a4824f404def257c6a65d917ee6cbd
HYAN DE SOUZA VENTURA <hyan_ventura@estudante.sesisenai.org.br>
2023-08-17 21:45:01 -0300

    feat: insert a new teacher to database

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const port = 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+const cadastrarRoutes = require("./routes/cadastrar")
+
+app.use('/cadastrar', cadastrarRoutes)
 
 
 app.listen(port, () =>{
