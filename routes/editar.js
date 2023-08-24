34862f2780267a13f46c61399cb924ad07f937cb
HYAN DE SOUZA VENTURA <hyan_ventura@estudante.sesisenai.org.br>
2023-08-24 19:44:53 -0300

    feat: edit a discipline

--- routes/editar.js
+++ routes/editar.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const controller = require('./../controllers/editar')
 
-router.put('/:cpf', controller.update)
+router.put('/professor/:cpf', controller.professor)
+router.put('/disciplina/:id', controller.disciplina)
 
 module.exports = router
