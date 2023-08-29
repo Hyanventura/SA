473d2a5dd7bcce07503efe93bb8e062b36654bf1
HYAN DE SOUZA VENTURA <hyan_ventura@estudante.sesisenai.org.br>
2023-08-28 21:08:51 -0300

    feat: edit a course

--- routes/editar.js
+++ routes/editar.js
@@ -6,5 +6,6 @@ router.put('/professor/:cpf', controller.professor)
 router.put('/disciplina/:id', controller.disciplina)
 router.put('/sala/:id', controller.sala)
 router.put('/turma/:id', controller.turma)
+router.put('/curso/:id',controller.curso)
 
 module.exports = router
