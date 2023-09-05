49f6ac97159a06adfb428d2da8d3964d57ca248a
HYAN DE SOUZA VENTURA <hyan_ventura@estudante.sesisenai.org.br>
2023-09-04 21:54:50 -0300

    feat : add the day of the week available in the teacher

--- controllers/cadastrar.js
+++ controllers/cadastrar.js
@@ -3,18 +3,32 @@ const database = require("../config/database");
 
 exports.professor = (req, res) => {
     const query = "INSERT INTO professores(cpf, nome,  status) VALUES ($1, $2, $3);";
-    const values = [req.body.cpf, req.body.nome, req.body.status];
+    const values = [req.body.cpf, req.body.nome, req.body.status,];
     database.query(query, values).then(
         () => {
-            console.log('ta dando insert')
+            disponibilidadeProfessor(req, res)
             return res.status(201).send({ mensagem: "Professor cadastrado com Sucesso!" });
         },
         (erro) => {
-            console.log('deu erro no insert')
             return res.status(500).send({ erro: erro });
         }
     )
-},
+
+
+}
+
+const disponibilidadeProfessor = (req, res) => {
+    const diasSemana = [req.body.seg, req.body.ter, req.body.qua, req.body.qui, req.body.sex]
+
+    for (i = 0; i < diasSemana.length; i++) {
+        if (diasSemana[i] == 1) {
+            const query = "INSERT INTO disponibilidade(cpf_professor, id_dia_semana) VALUES ($1, $2)"
+            const values = [req.body.cpf, (i+=2)]
+            database.query(query, values)
+        }
+    }
+
+}
 
 exports.disciplina = (req, res) => {
     const query = "INSERT INTO disciplinas(nome) VALUES ($1);";
@@ -27,7 +41,7 @@ exports.disciplina = (req, res) => {
             return res.status(500).send({ erro: erro });
         }
     )
-}   
+}
 
 exports.sala = (req, res) => {
     const query = "INSERT INTO salas(nome) VALUES ($1);";
@@ -40,7 +54,7 @@ exports.sala = (req, res) => {
             return res.status(500).send({ erro: erro });
         }
     )
-}   
+}
 
 exports.turma = (req, res) => {
     const query = "INSERT INTO turmas(nome, id_curso) VALUES ($1, $2);";
@@ -53,7 +67,7 @@ exports.turma = (req, res) => {
             return res.status(500).send({ erro: erro });
         }
     )
-}   
+}
 
 exports.curso = (req, res) => {
     const query = "INSERT INTO cursos(nome) VALUES ($1);";
